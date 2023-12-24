fd4b206c90a9a308074badddb89e88d573c0ab8b
Revi1337 <david122123@gmail.com>
2023-12-24 23:24:44 +0900

    initial commit

---
+++ src/test/java/revi1337/stockexample/StockExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package revi1337.stockexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
